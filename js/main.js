7a04f7e893c51bfb7dc35197ae09ecd430016023
Shubham Gautam <sg2199203@gmail.com>
2020-10-22 12:17:17 +0530

    Set Timings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Get rid of Preloader
-
 window.addEventListener('load', () => {
     var loader = document.querySelector(".loader"); //on class 'loader'
     loader.classList.add('load-finish');  
-})
+});
+//loader ends here
