b9704063dcb41b4357166a72b6c23c20688c135f
Shubham Gautam <sg2199203@gmail.com>
2020-10-22 14:03:07 +0530

    Navbar Developed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //Get rid of Preloader
-window.addEventListener('load', () => {
-    var loader = document.querySelector(".loader"); //on class 'loader'
-    loader.classList.add('load-finish');  
-});
+// window.addEventListener('load', () => {
+//     var loader = document.querySelector(".loader"); //on class 'loader'
+//     loader.classList.add('load-finish');  
+// });
 //loader ends here
+
