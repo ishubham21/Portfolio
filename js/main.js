8e1c33061de02b06ca8a0e4e978085f50088489e
Shubham Gautam <sg2199203@gmail.com>
2020-10-24 09:41:38 +0530

    Navbar Animations Added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Get rid of Preloader
-window.addEventListener('load', () => {
-    var loader = document.querySelector(".loader"); //on class 'loader'
-    loader.classList.add('load-finish');  
-});
+// window.addEventListener('load', () => {
+//     var loader = document.querySelector(".loader"); //on class 'loader'
+//     loader.classList.add('load-finish');  
+// });
 //loader ends here
 
 //on click ham-burger events
@@ -14,6 +14,7 @@ function navHam(){
 
     hamBurger.addEventListener("click", () =>{
         navMenu.classList.toggle("make-visible");
+        hamBurger.classList.toggle("cross-icon");    
     });
 
     for (let i = 0; i < navLinks.length; i++) {
